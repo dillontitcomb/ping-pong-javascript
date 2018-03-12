fb6e08b2628a54a0d0de3f393adb0fbb8b910244
dillon titcomb and tyler kostelak <tyler@>
2018-03-12 11:03:01 -0700

    webpack and javascript project adds form that takes number from user and returns info.

---
+++ src/ping-pong.js
@@ -0,0 +1,18 @@
+export function pingPong(goal) {
+  alert("Hey sucka");
+  console.log("output");
+  var output = [];
+  for (var i = 1; i <= goal; i++) {
+    if (i % 15 === 0) {
+      output.push("ping-pong");
+    } else if (i % 3 === 0) {
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
+    } else  {
+      output.push(i);
+    }
+  }
+  return output;
+
+}
